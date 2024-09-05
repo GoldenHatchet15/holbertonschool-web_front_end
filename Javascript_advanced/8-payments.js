8806e545b23bba83863e3722904e59154db87b12
GoldenHatchet15 <raphsant20@gmail.com>
2024-09-05 15:29:44 -0400

    payments

---
+++ Javascript_advanced/8-payments.js
@@ -0,0 +1,19 @@
+// Define the processPayment function
+function processPayment(amount) {
+    console.log(`Collecting payment of ${amount}`);
+  }
+  
+  // Define the processOrder function
+  function processOrder(orderId, amount) {
+    console.log(`${orderId} is being processed`);
+    processPayment(amount);
+    console.log(`${orderId} has been fully processed`);
+  }
+  
+  // Main part of the code
+  console.log('Processing orders');
+  processOrder(12321, 10.99);
+  processOrder(12322, 12.99);
+  processOrder(12323, 15.0);
+  console.log('All the orders have been processed');
+  
